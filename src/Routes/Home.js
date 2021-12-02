681a06e62c3083a89de27a9d019e323485d4b692
jeonbyeongmin <qudals7613@gmail.com>
2021-12-02 11:57:00 +0900

    update animation

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -3,13 +3,12 @@
  */
 import { useEffect, useRef, useState } from 'react';
 import styled, { keyframes } from 'styled-components';
-
 import { Container, Row, Col } from 'react-bootstrap';
 
 /*
  * import for firebase
  */
-// import { authService } from '../fbase';
+import { authService } from '../fbase';
 
 /*
  * import for Component
@@ -17,61 +16,8 @@ import { Container, Row, Col } from 'react-bootstrap';
 import ProductCard from '../Components/ProductCard';
 
 /*
- * Styled Component
+ * Keyframes
  */
-const Header = styled.header`
-  position: fixed;
-  top: 70px;
-  left: 0;
-  display: flex;
-  align-items: center;
-  width: 100%;
-  height: 70px;
-  background-color: #fff;
-  border-bottom: 1px solid #e8e8e8;
-  z-index: -1;
-  transition: all ease-out 0s;
-  /* opacity: 0; */
-
-  &.show {
-    z-index: 999;
-    opacity: 1;
-    transition: all ease-out 0.2s;
-  }
-  &.move {
-    z-index: 999;
-    opacity: 1;
-    transform: translateY(-70px);
-    transition: all ease-out 0.2s;
-  }
-
-  @media only screen and (max-width: 900px) {
-    width: 900px;
-  }
-`;
-
-const FlexBox = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const Gnb = styled.div`
-  display: flex;
-  align-items: center;
-  width: 100%;
-
-  font-weight: 700;
-`;
-
-const Pin = styled.span`
-  font-size: 30px;
-  margin-right: 8px;
-`;
-
-const Text = styled.span`
-  font-size: 20px;
-`;
-
 const CardAppear = keyframes`
     0% {
       opacity: 0;
@@ -90,6 +36,58 @@ const TitleAppear = keyframes`
       opacity: 1;
       transform: translateX(0);
     }
+`;
+
+/*
+ * Styled Component
+ */
+const Header = styled.header`
+  position: fixed;
+  top: 70px;
+  left: 0;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 70px;
+  background-color: #fff;
+  border-bottom: 1px solid #e8e8e8;
+  z-index: -1;
+  transition: all ease-out 0s;
+
+  &.show {
+    z-index: 999;
+    transition: all ease-out 0.2s;
+  }
+  &.move {
+    z-index: 999;
+    transform: translateY(-70px);
+    transition: all ease-out 0.2s;
+  }
+
+  @media only screen and (max-width: 900px) {
+    width: 900px;
+  }
+`;
+
+const FlexBox = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Gnb = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  font-weight: 700;
+`;
+
+const Pin = styled.span`
+  font-size: 30px;
+  margin-right: 8px;
+`;
+
+const Text = styled.span`
+  font-size: 20px;
 `;
 
 const TitleContainer = styled(Container)`
@@ -150,6 +148,7 @@ const PostingButton = styled.button`
   width: 140px;
   height: 45px;
   margin-bottom: 50px;
+  margin-right: 50px;
   border-radius: 25px;
   transition: all ease-out 0.2s;
 
@@ -161,6 +160,18 @@ const PostingButton = styled.button`
     opacity: 1;
     background-color: lightgray;
     transition: all ease-out 0.2s;
+  }
+
+  @media only screen and (max-width: 992px) {
+    font-size: 15px;
+    font-weight: 600;
+    margin-left: 20px;
+  }
+
+  @media only screen and (max-width: 768px) {
+    font-size: 14px;
+    font-weight: 600;
+    margin-bottom: 0px;
   }
 `;
 
@@ -191,6 +202,9 @@ const throttle = function (callback, waitTime) {
   };
 };
 
+/*
+ * Home Component
+ */
 const Home = ({ data }) => {
   const [products, setProducts] = useState(data);
 
