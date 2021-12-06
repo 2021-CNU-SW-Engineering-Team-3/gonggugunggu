1dc34dfa122f84d2c50dbb26c61213f8e8a03d6c
gooriiie <fkstldghks123@naver.com>
2021-12-06 15:23:14 +0900

    chore. userList css

--- src/Components/User.js
+++ src/Components/User.js
@@ -4,36 +4,50 @@ import styled from 'styled-components';
 
 import { Link } from 'react-router-dom';
 
-const UserInfo = styled.div`
-  flex: 1;
-  padding: 1rem;
-  display: flex;
-  align-items: center;
-  cursor: pointer;
-  transition: all 0.15s;
-  user-select: none;
-  &:hover {
-    background-color: #e3fafc;
-  }
-  width: 800px;
+const TableBodyColumn = styled.td`
+    padding: 10px 5px;
+`;
+
+const TableBodyRow = styled.tr`
+    &:hover{
+        background-color: #eceaea;
+        cursor: pointer;
+    }
 `;
 
 const User = ({ users }) => {
 
   return (
-      <div>
-        {users.map(user => {
-          return (
-            <div class="card mb-3" key={user.id}>
-              <UserInfo >
-                <Link to={`/userDetail/${user.id}`}>
-                  {user.name} {user.email}
-                </Link>
-              </UserInfo>
-            </div>
-          );
-        })}
-      </div>
+        <tbody>
+            {
+                users.map((user, index) => {
+                    return (
+                        <TableBodyRow>
+                            <TableBodyColumn>{index}</TableBodyColumn>
+                            <TableBodyColumn>
+                                <Link to={`/userDetail/${user.id}`}>
+                                    {user.name}
+                                </Link>
+                            </TableBodyColumn>
+                            <TableBodyColumn>{user.email}</TableBodyColumn>
+                        </TableBodyRow>
+                    )
+                })
+            }
+        </tbody>
+      // <div>
+      //   {users.map(user => {
+      //     return (
+      //       <div class="card mb-3" key={user.id}>
+      //         <UserInfo >
+      //           <Link to={`/userDetail/${user.id}`}>
+      //             {user.name} {user.email}
+      //           </Link>
+      //         </UserInfo>
+      //       </div>
+      //     );
+      //   })}
+      // </div>
   );
   
   // return (
