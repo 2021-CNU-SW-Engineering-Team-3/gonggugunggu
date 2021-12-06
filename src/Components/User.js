567e37cb3119a8b9b69c7a0dcdc21c6c150ebbd6
gooriiie <fkstldghks123@naver.com>
2021-12-06 15:36:45 +0900

    chore. userList css

--- src/Components/User.js
+++ src/Components/User.js
@@ -4,52 +4,38 @@ import styled from 'styled-components';
 
 import { Link } from 'react-router-dom';
 
-const UserInfo = styled.div`
-  flex: 1;
-  padding: 1rem;
-  display: flex;
-  align-items: center;
-  cursor: pointer;
-  transition: all 0.15s;
-  user-select: none;
-  &:hover {
-    background-color: #e3fafc;
-  }
-  width: 800px;
+const TableBodyColumn = styled.td`
+    padding: 10px 5px;
+`;
+
+const TableBodyRow = styled.tr`
+    &:hover{
+        background-color: #eceaea;
+        cursor: pointer;
+    }
 `;
 
 const User = ({ users }) => {
 
   return (
-      <div>
-        {users.map(user => {
-          return (
-            <div class="card mb-3" key={user.id}>
-              <UserInfo >
-                <Link to={`/userDetail/${user.id}`}>
-                  {user.name} {user.email}
-                </Link>
-              </UserInfo>
-            </div>
-          );
-        })}
-      </div>
-  );
-  
-  // return (
-  //   <div>
-  //     {users.map(user => {
-  //       return (
-  //         <div className="card mb-2" key={user.id}>
-  //           <div className="card-body p-3">
-  //             {user.id} {user.email}
-  //           </div>
-  //         </div>
-  //       );
-  //     })}
-  //   </div>
-  // );
-  
+        <tbody>
+            {
+                users.map((user, index) => {
+                    return (
+                        <TableBodyRow>
+                            <TableBodyColumn>{index}</TableBodyColumn>
+                            <TableBodyColumn>
+                                <Link to={`/userDetail/${user.id}`}>
+                                    {user.name}
+                                </Link>
+                            </TableBodyColumn>
+                            <TableBodyColumn>{user.email}</TableBodyColumn>
+                        </TableBodyRow>
+                    )
+                })
+            }
+        </tbody>
+  )
 };
 
 export default User;
